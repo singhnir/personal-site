c39fe218ad90e1ff17f4ba9415364861320e0d2c
singhnir <hinduites2016@gmail.com>
2025-08-24 18:21:47 +0530

    Deploy to GitHub Pages

--- src/components/Template/SideBar.tsx
+++ src/components/Template/SideBar.tsx
@@ -15,26 +15,21 @@ const SideBar: React.FC = () => {
     <section id="sidebar">
       <section id="intro">
         <Link href="/" className="logo">
-          <Image src="/images/me.jpg" alt="Michael D'Angelo" width={200} height={200} priority />
+          <Image src="/images/me.jpg" alt="Sushmita" width={200} height={200} priority />
         </Link>
         <header>
-          <h2>Michael D&apos;Angelo</h2>
-          <p>
-            <a href="mailto:michael@mldangelo.com">michael@mldangelo.com</a>
-          </p>
-        </header>
+        <h2>Sushmita</h2>
+        <p>
+          <a href="mailto:sushmita7988@gmail.com">sushmita7988@gmail.com</a>
+        </p>
+      </header>
       </section>
 
       <section className="blurb">
         <h2>About</h2>
         <p>
-          Hi, I&apos;m Michael. I am a <a href="https://icme.stanford.edu/">Stanford ICME</a>{' '}
-          graduate, YC alumnus, and the co-founder and CTO of{' '}
-          <a href="https://promptfoo.dev">Promptfoo</a>. Previously, I was VP of Engineering at{' '}
-          <a href="https://usesmileid.com">SmileID</a>, co-founder and CTO of{' '}
-          <a href="https://arthena.com">Arthena</a>, and co-founded{' '}
-          <a href="https://matroid.com">Matroid</a>.
-        </p>
+        Hi, I&apos;m Sushmita, SAP Developer at BCH Electric Ltd. B.Tech in IT from NIT Kurukshetra.
+      </p>
         <ul className="actions">
           <li>
             {pathname && !pathname.includes('/resume') ? (
@@ -53,7 +48,7 @@ const SideBar: React.FC = () => {
       <section id="footer">
         <ContactIcons />
         <p className="copyright">
-          &copy; Michael D&apos;Angelo <Link href="/">mldangelo.com</Link>.
+          &copy; Sushmita <Link href="/">cloudsofsushi.github.io</Link>.
         </p>
       </section>
     </section>
